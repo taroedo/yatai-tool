47f81c31e611216080fbbfa7c082432ee757f4cf
taroedo <taroedo@gmail.com>
2026-08-19 21:43:03 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDOzbf7UYN2TDgFbC45Joy_hIn5iaYGFaiI",
+  authDomain: "shino4-yatai-2026.firebaseapp.com",
+  databaseURL: "https://shino4-yatai-2026-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shino4-yatai-2026",
+  appId: "1:416010786613:web:c8c97f61d6ac299acd9e7c"
+};
+
+// イベントごとの識別文字列
+export const eventId = "shino4-yatai-2026-k7p9m2x4q8v6";
+
+// 管理画面を開く簡易PIN
+export const adminPin = "4826";
